8bb6b6c778ac5c60605bca5db5fdcc27123b6512
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-17 17:10:11 -0300

    feat: adicionando busca páginada..

--- src/main/java/com/crud/client/controllers/ClientController.java
+++ src/main/java/com/crud/client/controllers/ClientController.java
@@ -2,7 +2,9 @@ package com.crud.client.controllers;
 
 import com.crud.client.dto.ClientDTO;
 import com.crud.client.services.ClientService;
+import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,12 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable  Long id) {
         ClientDTO clientDTO = clientService.findById(id);
         return ResponseEntity.ok().body(clientDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> clientDTOPage = clientService.findAll(pageable);
+        return ResponseEntity.ok(clientDTOPage);
     }
 
 
--- src/main/java/com/crud/client/services/ClientService.java
+++ src/main/java/com/crud/client/services/ClientService.java
@@ -5,6 +5,8 @@ import com.crud.client.entities.Client;
 import com.crud.client.repositories.ClientRepository;
 import com.crud.client.services.exceptions.ResoruceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +15,6 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    //Busca paginada de recursos
-
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
          Client client = clientRepository.findById(id).orElseThrow(
@@ -22,6 +22,13 @@ public class ClientService {
         );
 
          return new ClientDTO(client);
+    }
+
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> clients = clientRepository.findAll(pageable);
+        return clients.map(x -> new ClientDTO(x));
     }
 
 
