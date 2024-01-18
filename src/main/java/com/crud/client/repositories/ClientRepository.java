c064f914e5654b8f7aca9fb3c1ef97cc3b764ae3
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-17 23:39:56 -0300

    feat: migrando para mysql com docker

--- src/main/java/com/crud/client/repositories/ClientRepository.java
+++ src/main/java/com/crud/client/repositories/ClientRepository.java
@@ -3,6 +3,9 @@ package com.crud.client.repositories;
 import com.crud.client.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Pedro Henrique', '107.822.289-01', 222.0, '2000-05-21', 2);
-INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Lorena', '106.831.249-02', 222.0, '2004-01-22', 15);
+INSERT INTO TB_client(name, cpf, income, birth_date, children) VALUES ('Lorena', '106.831.249-02', 222.0, '2004-01-22', 15);
